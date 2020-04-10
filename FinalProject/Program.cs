74f962a6cbdb1c202e79100a9eff0b97b06d2bb1
spenc <sdowty@umassd.edu>
2020-04-10 12:23:55 -0400

    LD commit

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -5,16 +5,40 @@ namespace FinalProject
 {
     class Program
     {
+        static List<AdventureRoom> myRooms;
+
         static void Main(string[] args)
         {
+            myRooms = new List<AdventureRoom>(140);
             getData(@"..\..\..\rooms.txt");
+
+            Console.WriteLine(myRooms[4].Description);
         }
 
         static void getData(string filename)
         {
             using (StreamReader fs = new StreamReader(filename))
             {
-                Console.WriteLine(fs.ReadLine());
+                fs.ReadLine(); //section 1
+                //Console.WriteLine(fs.ReadLine());
+                while (true)
+                {
+                    string tmp = fs.ReadLine();
+                    string[] q = tmp.split('\t');
+                    int roomNumber = int.parse(q[0]);
+
+                    if(roomNumber<0)
+                    {
+                        break;
+                    }
+
+                    if (myRooms.Count < roomNumber)
+                    {
+                        myRooms.Add(new AdventureRoom());
+                    }
+                    myRooms[roomNumber].Description += q[1];
+                    
+                }
             }
         }
     }
---
+++ FinalProject/Section_1_LD.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProject
+{
+    public class AdventureRoom
+    {
+        private string _Description;
+
+        public AdventureRoom(string d)
+        {
+            Description = d;
+        }
+
+        public AdventureRoom() : this("")
+        { }
+
+        public string Description
+        {
+            get { return _Description; }
+            set { _Description = value; }
+        }
+
+
+    }
+}
