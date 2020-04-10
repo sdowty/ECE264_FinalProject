b64b914a13d86fcf8b07510921ab02c47f609f67
spenc <sdowty@umassd.edu>
2020-04-10 13:18:21 -0400

    Short & Long

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Text;
 
 namespace FinalProject
 {
@@ -12,33 +14,54 @@ namespace FinalProject
             myRooms = new List<AdventureRoom>(140);
             getData(@"..\..\..\rooms.txt");
 
-            Console.WriteLine(myRooms[4].Description);
+            myRooms[8].Print();
         }
 
         static void getData(string filename)
         {
+            string tmp;
             using (StreamReader fs = new StreamReader(filename))
             {
                 fs.ReadLine(); //section 1
+                myRooms.Add(new AdventureRoom());
                 //Console.WriteLine(fs.ReadLine());
                 while (true)
                 {
-                    string tmp = fs.ReadLine();
-                    string[] q = tmp.split('\t');
-                    int roomNumber = int.parse(q[0]);
+                    // get room descriptions
+
+                    tmp = fs.ReadLine();
+                    string[] q = tmp.Split('\t');
+                    int roomNumber = int.Parse(q[0]);
 
                     if(roomNumber<0)
                     {
                         break;
                     }
 
-                    if (myRooms.Count < roomNumber)
+                    if (myRooms.Count < roomNumber + 1)
                     {
                         myRooms.Add(new AdventureRoom());
                     }
-                    myRooms[roomNumber].Description += q[1];
-                    
+                    if (myRooms[roomNumber].Description.Length == 0)
+                        myRooms[roomNumber].Description = q[1];
+                    else
+                        myRooms[roomNumber].Description += " " + q[1];
+                    // done with room descriptions
                 }
+
+                tmp = fs.ReadLine(); //start section 2
+                while (true)
+                {
+                    tmp = fs.ReadLine();
+                    string[] q = tmp.Split('\t');
+                    int roomNumber = int.Parse(q[0]);
+
+                    if (roomNumber < 0)
+                        break;
+                    myRooms[roomNumber].ShortDescription = q[1];
+                }
+
+                Console.WriteLine(fs.ReadLine());
             }
         }
     }
--- FinalProject/Section_1_LD.cs
+++ FinalProject/Section_1_LD.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace FinalProject
 {
     public class AdventureRoom
     {
         private string _Description;
+        private string _ShortDescription;
 
         public AdventureRoom(string d)
         {
@@ -22,6 +24,34 @@ namespace FinalProject
             set { _Description = value; }
         }
 
+        public string ShortDescription
+        {
+            get;
+            set;
+        }
+
+        public int GetDescription(StreamReader sr)
+        {
+            string tmp = sr.ReadLine();
+            string[] q = tmp.Split('\t');
+            int roomNumber = int.Parse(q[0]);
+
+            if(roomNumber < 0)
+                return roomNumber;
+
+            if (Description.Length == 0)
+                Description = q[1];
+            else
+                Description += " " + q[1];
+
+            return roomNumber;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Short: {0}", ShortDescription);
+            Console.WriteLine("Long: {0}", Description);
+        }
 
     }
 }
